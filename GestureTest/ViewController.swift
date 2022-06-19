158087b4d9c7553e4fec400b65d57ed838096e3f
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-06-19 20:54:18 +0300

    first commit

--- GestureTest/ViewController.swift
+++ GestureTest/ViewController.swift
@@ -8,11 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isMatrix = true
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
+    
+    @objc func changePic() {
+        
+       
+        
+        if isMatrix == true {
+            imageView.image = UIImage(named: "matrix2")
+            myLabel.text = "Matrix 2 'ye geçtiniz.."
+            isMatrix = false
+        } else {
+            imageView.image = UIImage(named: "matrix")
+            myLabel.text = "Matrix 1 'e geçtiniz.."
+            isMatrix = true
+        }
+        
+       
+        
+        
+    }
+    
+
 
 
 }
