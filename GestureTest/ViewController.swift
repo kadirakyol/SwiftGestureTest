7b1b69097683576accca04a7e4ae97f0f22a7652
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-06-19 20:24:28 +0300

    Initial Commit

---
+++ GestureTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureTest
+//
+//  Created by Kadir Akyol on 19.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
